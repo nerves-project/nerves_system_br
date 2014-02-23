a4cfefd724932e902b712acf651110152d31bfd9
Frank Hunleth <fhunleth@troodon-software.com>
2014-02-22 21:00:58 -0500

    erlang-relx: switch to prebuilt release
    Erlware publishes pre-built versions of relx on Github, so use those
    instead of building relx from scratch.

--- package/erlang-relx/erlang-relx.mk
+++ package/erlang-relx/erlang-relx.mk
@@ -4,23 +4,23 @@
 #
 #############################################################
 
-ERLANG_RELX_VERSION = 10620765c5028c24d57caba20b8e86b98351e3a5
-ERLANG_RELX_SITE = $(call github,erlware,relx,$(ERLANG_RELX_VERSION))
+ERLANG_RELX_VERSION = v0.6.0
+ERLANG_RELX_SITE = https://github.com/erlware/relx/releases/download/$(ERLANG_RELX_VERSION)
+ERLANG_RELX_SOURCE = relx
 ERLANG_RELX_LICENSE = Apache-2.0
-ERLANG_RELX_LICENSE_FILE = LICENSE.md
-
-HOST_ERLANG_RELX_DEPENDENCIES = host-erlang host-erlang-rebar
+HOST_ERLANG_RELX_DEPENDENCIES = host-erlang
 
 # Macro for invoking relx in other packages
 RELX = $(HOST_MAKE_ENV) \
 	$(HOST_DIR)/usr/bin/relx -l $(STAGING_DIR)/usr/lib/erlang/lib
 
-define HOST_ERLANG_RELX_BUILD_CMDS
-	$(HOST_MAKE_ENV) $(MAKE1) -C $(@D)
+define HOST_ERLANG_RELX_EXTRACT_CMDS
+	cp $(DL_DIR)/$(ERLANG_RELX_SOURCE) $(@D)
 endef
 
 define HOST_ERLANG_RELX_INSTALL_CMDS
-	cp $(@D)/relx $(HOST_DIR)/usr/bin
+	$(INSTALL) -m 0755 -D $(@D)/$(ERLANG_RELX_SOURCE) \
+		$(HOST_DIR)/usr/bin/relx
 endef
 
 $(eval $(host-generic-package))
