3438b81ddee6aaec975f363c6a7b2d20cee9283d
Garth Hitchens <ghitchens@me.com>
2014-02-27 07:45:43 -0800

    More elixir support

--- package/elixir/elixir.mk
+++ package/elixir/elixir.mk
@@ -1,12 +1,12 @@
 #############################################################
 #
-# erlang-elixir
+# elixir
 #
 #############################################################
 
-ERLANG_ELIXIR_VERSION = v0.12.4
-ERLANG_ELIXIR_SITE = $(call github,elixir-lang,elixir,$(ERLANG_ELIXIR_VERSION))
-ERLANG_ELIXIR_LICENSE = Apache-2.0
+ELIXIR_VERSION = v0.12.4
+ELIXIR_SITE = $(call github,elixir-lang,elixir,$(ELIXIR_VERSION))
+ELIXIR_LICENSE = Apache-2.0
 HOST_ERLANG_ELIXIR_DEPENDENCIES = host-erlang
 
 $(eval $(autotools-package))
