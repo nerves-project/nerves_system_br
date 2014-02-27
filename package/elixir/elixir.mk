ac580c3d705f9a5d4276b4e1623c654312da6a92
Garth Hitchens <ghitchens@me.com>
2014-02-27 07:43:39 -0800

    elixir: add host-elixir support

---
+++ package/elixir/elixir.mk
@@ -0,0 +1,25 @@
+#############################################################
+#
+# elixir
+#
+#############################################################
+
+ELIXIR_VERSION = v0.12.4
+ELIXIR_SITE = $(call github,elixir-lang,elixir,$(ELIXIR_VERSION))
+ELIXIR_LICENSE = Apache-2.0
+ELIXIR_LICENSE_FILES = LICENSE
+
+# Technically we only depend on host-erlang to build the elixir
+# compiler, but the files built by it only run if erlang is on
+# the target.
+HOST_ELIXIR_DEPENDENCIES = host-erlang erlang
+
+define HOST_ELIXIR_BUILD_CMDS
+	$(MAKE) -C $(@D)
+endef
+
+define HOST_ELIXIR_INSTALL_CMDS
+	$(MAKE) -C $(@D) PREFIX=/usr DESTDIR=$(HOST_DIR) install
+endef
+
+$(eval $(host-generic-package))
