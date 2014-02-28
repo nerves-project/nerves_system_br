290c662cb2a8885d34ad458751e46e7ce024e330
Frank Hunleth <fhunleth@troodon-software.com>
2014-02-28 02:39:04 -0500

    mmccopy: update to v1.0.1

--- package/mmccopy/mmccopy.mk
+++ package/mmccopy/mmccopy.mk
@@ -4,10 +4,11 @@
 #
 #############################################################
 
-MMCCOPY_VERSION = v1.0.0
+MMCCOPY_VERSION = v1.0.1
 MMCCOPY_SITE = $(call github,fhunleth,mmccopy,$(MMCCOPY_VERSION))
 MMCCOPY_LICENSE = MIT
 MMCCOPY_LICENSE_FILES = COPYING
+MMCCOPY_AUTORECONF = YES
 
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
