f6791debf973841de19904656682af99ce4028b7
Garth Hitchens <ghitchens@me.com>
2014-02-27 18:13:52 -0800

    more elixir support

--- package/elixir/elixir.mk
+++ package/elixir/elixir.mk
@@ -7,9 +7,16 @@
 ELIXIR_VERSION = v0.12.4
 ELIXIR_SITE = $(call github,elixir-lang,elixir,$(ELIXIR_VERSION))
 ELIXIR_LICENSE = Apache-2.0
-HOST_ERLANG_ELIXIR_DEPENDENCIES = host-erlang
 
-$(eval $(autotools-package))
+HOST_ELIXIR_DEPENDENCIES = host-erlang
+
+define HOST_ELIXIR_CONFIGURE_CMDS
+	# Elixir has no configure script so no Configuring needed
+endef
+
+define HOST_ELIXIR_INSTALL_CMDS
+	cd $(@D); make PREFIX=/usr DESTDIR=$(HOST_DIR) install
+endef
+
 $(eval $(host-autotools-package))
-
-# $(eval $(host-generic-package))
+#$(eval $(autotools-package))
