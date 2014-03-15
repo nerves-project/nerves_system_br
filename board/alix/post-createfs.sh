c0d6d1fa0d5a13ec562ee6e39b83a8b3149dbc47
root <root@debian.local>
2014-03-15 08:27:03 -0700

    ag150 -> alix

--- board/alix/post-createfs.sh
+++ board/alix/post-createfs.sh
@@ -10,7 +10,7 @@ NERVES_ROOT=$TARGETDIR/../../..
 NERVES_SDK_ROOT=$TARGETDIR/../host
 NERVES_SDK_IMAGES=$TARGETDIR/../images
 
-FWTOOL_CONFIG=$NERVES_ROOT/board/ag150/fwtool.config
+FWTOOL_CONFIG=$NERVES_ROOT/board/alix/fwtool.config
 
 # Create the firmware images
 FWTOOL=$NERVES_SDK_ROOT/usr/bin/fwtool
@@ -20,15 +20,15 @@ echo Building firmware image...
 PATH=$NERVES_SDK_ROOT/usr/bin:$NERVES_SDK_ROOT/usr/sbin:$PATH $FWTOOL \
 	-c $FWTOOL_CONFIG \
 	--base_path=$NERVES_ROOT \
-	create $NERVES_SDK_IMAGES/nerves-ag150.fw
+	create $NERVES_SDK_IMAGES/nerves-alix.fw
 
 # Build the raw image for the bulk programmer
 echo Building raw firmware image...
 PATH=$NERVES_SDK_ROOT/usr/bin:$NERVES_SDK_ROOT/usr/sbin:$PATH $FWTOOL \
 	-c $FWTOOL_CONFIG \
-	-d $NERVES_SDK_IMAGES/ag150.img \
+	-d $NERVES_SDK_IMAGES/alix.img \
 	-t complete \
-	run $NERVES_SDK_IMAGES/nerves-ag150.fw
+	run $NERVES_SDK_IMAGES/nerves-alix.fw
 
 # Link the fwtool config to the images directory so that
 # it can be used to create images based on this one.
