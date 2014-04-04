1ca4466dbe99cd394b65b5621c72502888ed7341
Garth Hitchens <ghitchens@me.com>
2014-04-04 11:30:28 -0700

    resolved merge issue

--- nerves-env.sh
+++ nerves-env.sh
@@ -18,55 +18,7 @@ fi
 
 NERVES_ROOT=$(dirname $(readlink -f $BASH_SOURCE))
 
-# Check that the base buildroot image has been built
-[ -d "$NERVES_ROOT/buildroot/output" ] || { echo "ERROR: Run \"make\" first to build the nerves SDK."; return 1; }
-
-# Past the checks, so export variables
-export NERVES_ROOT
-export NERVES_SDK_ROOT
-export NERVES_SDK_IMAGES
-export NERVES_SDK_SYSROOT
-
-# Rebar environment variables
-PLATFORM_DIR=$NERVES_ROOT/sdk/$NERVES_PLATFORM
-
-ERTS_DIR=`ls -d $NERVES_SDK_SYSROOT/usr/lib/erlang/erts-*`
-ERL_INTERFACE_DIR=`ls -d $NERVES_SDK_SYSROOT/usr/lib/erlang/lib/erl_interface-*`
-CROSSCOMPILE=`ls $NERVES_SDK_ROOT/usr/bin/*gcc | grep "\-buildroot" | sed -e s/-gcc//`
-
-
-export REBAR_PLT_DIR=$NERVES_SDK_SYSROOT/usr/lib/erlang
-export CC=$CROSSCOMPILE-gcc
-export CXX=$CROSSCOMPILE-g++
-export CFLAGS="-D_LARGEFILE_SOURCE -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64  -pipe -Os"
-export CXXFLAGS="-D_LARGEFILE_SOURCE -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64  -pipe -Os"
-export LDFLAGS=""
-export ERL_CFLAGS="-I$ERTS_DIR/include -I$ERL_INTERFACE_DIR/include"
-export ERL_LDFLAGS="-L$ERTS_DIR/lib -L$ERL_INTERFACE_DIR/lib -lerts -lei"
-export ERL_EI_LIBDIR="$ERL_INTERFACE_DIR/lib"
-export STRIP=$CROSSCOMPILE-strip
-
-export PKG_CONFIG=$NERVES_SDK_ROOT/usr/bin/pkg-config
-export PKG_CONFIG_SYSROOT_DIR=/
-export PKG_CONFIG_LIBDIR=$NERVES_SDK_ROOT/usr/lib/pkgconfig
-export PERLLIB=$NERVES_SDK_ROOT/usr/lib/perl
-
-pathadd() {
-    if [ -d "$1" ] && [[ ":$PATH:" != *":$1:"* ]]; then
-        PATH="$1:$PATH"
-    fi
-}
-
-ldlibrarypathadd() {
-    if [ -d "$1" ] && [[ ":$LD_LIBRARY_PATH:" != *":$1:"* ]]; then
-        LD_LIBRARY_PATH="$1:$LD_LIBRARY_PATH"
-    fi
-}
-
-pathadd $NERVES_SDK_ROOT/usr/bin
-pathadd $NERVES_SDK_ROOT/usr/sbin
-pathadd $NERVES_SDK_ROOT/bin
-ldlibrarypathadd $NERVES_SDK_ROOT/usr/lib
+source $NERVES_ROOT/scripts/nerves-env-helper.sh $NERVES_ROOT
 
 echo Shell environment updated for nerves
 echo Cross-compiler prefix: `basename $CROSSCOMPILE`
