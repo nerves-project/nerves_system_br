30619030dd67074283c14f93601dbf4215aa5a00
Garth Hitchens <ghitchens@me.com>
2014-04-04 13:28:45 -0700

    incompatibility fix with old basename

--- scripts/rel2fw.sh
+++ scripts/rel2fw.sh
@@ -23,7 +23,7 @@ OUTPUT_DIR=$BASE_DIR/_images
 [ -z "$NERVES_ROOT" ] && { echo "Error: Source nerves-env.sh and try again."; exit 1; }
 [ ! -d "$RELEASE_DIR" ] && { echo "Error: Check that your app's release directory exists. ($RELEASE_DIR)"; exit 1; }
 [ -z "$FW_FILENAME" ] && FW_FILENAME=${PROJECT_DIR}.fw
-[ -z "$IMG_FILENAME" ] && IMG_FILENAME=`basename .fw $FW_FILENAME`.img
+[ -z "$IMG_FILENAME" ] && IMG_FILENAME=`basename $FW_FILENAME .fw`.img
 
 command -v fakeroot >/dev/null 2>&1 || { echo "Error: This script requires fakeroot." >&2; exit 1; }
 
