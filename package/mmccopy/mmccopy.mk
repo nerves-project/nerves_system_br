8389528dc209670a61435130c6eb183ef832b2a6
Frank Hunleth <fhunleth@troodon-software.com>
2014-04-22 21:09:01 -0400

    mmccopy: add sudo-mmccopy script
    This addresses the problem of it not being easy to run mmccopy under
    sudo.

--- package/mmccopy/mmccopy.mk
+++ package/mmccopy/mmccopy.mk
@@ -10,5 +10,11 @@ MMCCOPY_LICENSE = MIT
 MMCCOPY_LICENSE_FILES = COPYING
 MMCCOPY_AUTORECONF = YES
 
+define HOST_MMCCOPY_INSTALL_SUDO_HELPER
+	$(INSTALL) -D -m 755 ../package/mmccopy/sudo-mmccopy $(HOST_DIR)/usr/bin/sudo-mmccopy
+endef
+
+HOST_MMCCOPY_POST_INSTALL_HOOKS += HOST_MMCCOPY_INSTALL_SUDO_HELPER
+
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
