374cce6ac61b82ce7c2b50fd34ba3d19669c7b53
Frank Hunleth <fhunleth@troodon-software.com>
2014-06-13 11:24:17 -0400

    mmccopy: bump to version v1.1.1
    This fixes the progress calculation on 32-bit machines.

--- package/mmccopy/mmccopy.mk
+++ package/mmccopy/mmccopy.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-MMCCOPY_VERSION = v1.1.0
+MMCCOPY_VERSION = v1.1.1
 MMCCOPY_SITE = $(call github,fhunleth,mmccopy,$(MMCCOPY_VERSION))
 MMCCOPY_LICENSE = MIT
 MMCCOPY_LICENSE_FILES = COPYING
