f5c83823215ff734c886a7ee83ecf4aebbeb107a
Frank Hunleth <fhunleth@troodon-software.com>
2014-08-30 18:50:01 -0400

    Update BBB 3.8 kernel and add 3.14 kernel
    The 3.14 kernel support is very lightly tested, but boots.

--- board/bbb/update-kernel-patch.sh
+++ board/bbb/update-kernel-patch.sh
@@ -2,20 +2,31 @@
 
 set -e
 
-# Run this script to update the BBB kernel's patch. You'll need to
-# update the KERNEL_VERSION variable based on RCN's latest release.
+# Run this script to update the BBB kernel patches. See the bottom
+# of the script for setting the versions.
+
+update_kernel_patch() {
+  local KERNEL_VERSION=$1
+  local OUTPUT_PATCH=$2
+
+  # Download the master patch file for the RCN kernel
+  local ORIGINAL_DIFF_GZ=patch-$KERNEL_VERSION.diff.gz
+  rm -f $ORIGINAL_DIFF_GZ
+  wget http://rcn-ee.net/deb/sid-armhf/v$KERNEL_VERSION/$ORIGINAL_DIFF_GZ
+
+  # Expand the compressed diff and filter it through filterdiff.
+  # Since buildroot uses patch instead of "git am" to apply diffs,
+  # the diff file needs to be compatible. The git diff from RCN
+  # is close, but contains one GIT binary diff that causes buildroot
+  # to fail.
+  gunzip -c $ORIGINAL_DIFF_GZ | filterdiff > $OUTPUT_PATCH
+
+  # Clean up
+  rm -f $ORIGINAL_DIFF_GZ
+}
+
 KERNEL_VERSION=3.8.13-bone64
+update_kernel_patch 3.8.13-bone64 rcn-linux-kernel-3.8.patch
+update_kernel_patch 3.14.17-bone8 rcn-linux-kernel-3.14.patch
 
-# Download the master patch file for the RCN kernel
-ORIGINAL_DIFF_GZ=patch-$KERNEL_VERSION.diff.gz
-rm -f $ORIGINAL_DIFF_GZ
-wget http://rcn-ee.net/deb/sid-armhf/v$KERNEL_VERSION/$ORIGINAL_DIFF_GZ
-
-# Expand the compressed diff and filter it through filterdiff.
-# Since buildroot uses patch instead of "git am" to apply diffs,
-# the diff file needs to be compatible. The git diff from RCN
-# is close, but contains one GIT binary diff that causes buildroot
-# to fail.
-gunzip -c $ORIGINAL_DIFF_GZ | filterdiff > rcn-linux-kernel.patch
-
-echo Updated rcn-linux-kernel.patch. Now rebuild the linux kernel.
+echo Updated patches. Now rebuild the linux kernel.
