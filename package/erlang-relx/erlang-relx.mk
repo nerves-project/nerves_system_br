de118a29d06d6a74877964b96afcd8e599e454be
Frank Hunleth <fhunleth@troodon-software.com>
2015-01-24 19:41:08 -0500

    erlang-relx: bump to version v1.1.0
    Also improved convenience macro for pulling in the cross-compiled system
    libraries.

--- package/erlang-relx/erlang-relx.mk
+++ package/erlang-relx/erlang-relx.mk
@@ -4,15 +4,19 @@
 #
 #############################################################
 
-ERLANG_RELX_VERSION = v0.6.0
+ERLANG_RELX_VERSION = v1.1.0
 ERLANG_RELX_SITE = https://github.com/erlware/relx/releases/download/$(ERLANG_RELX_VERSION)
 ERLANG_RELX_SOURCE = relx
 ERLANG_RELX_LICENSE = Apache-2.0
 HOST_ERLANG_RELX_DEPENDENCIES = host-erlang
 
-# Macro for invoking relx in other packages
+# Use $(RELX) to build releases for the target
 RELX = $(HOST_MAKE_ENV) \
-	$(HOST_DIR)/usr/bin/relx -l $(STAGING_DIR)/usr/lib/erlang/lib
+	$(HOST_DIR)/usr/bin/relx --system_libs $(STAGING_DIR)/usr/lib/erlang/lib -o $(@D)/_rel
+
+# Use $(HOST_RELX) for building host releases
+HOST_RELX = $(HOST_MAKE_ENV) \
+	$(HOST_DIR)/usr/bin/relx -o $(@D)/_rel
 
 define HOST_ERLANG_RELX_EXTRACT_CMDS
 	cp $(DL_DIR)/$(ERLANG_RELX_SOURCE) $(@D)
