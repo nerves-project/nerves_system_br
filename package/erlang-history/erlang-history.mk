3e4aec4e1a4ea1d2747ff44491cb2001da6bc6ee
Frank Hunleth <fhunleth@troodon-software.com>
2015-02-10 13:24:27 -0500

    erlang-history: new package

---
+++ package/erlang-history/erlang-history.mk
@@ -0,0 +1,32 @@
+#############################################################
+#
+# erlang-history
+#
+#############################################################
+
+ERLANG_HISTORY_VERSION = b03c79d6bc0b27a4b1ea95cc0ff1b2260556f7c1
+ERLANG_HISTORY_SITE = $(call github,ferd,erlang-history,$(ERLANG_HISTORY_VERSION))
+ERLANG_HISTORY_DEPENDENCIES = erlang host-erlang
+ERLANG_HISTORY_INSTALL_STAGING = YES
+ERLANG_HISTORY_INSTALL_TARGET = NO
+
+define ERLANG_HISTORY_BUILD_CMDS
+	cd $(@D) && $(HOST_DIR)/usr/bin/erl -make
+endef
+
+ERLANG_KERNEL_VSN = $(firstword $(patsubst $(HOST_DIR)/usr/lib/erlang/lib/kernel-%,%,$(wildcard $(HOST_DIR)/usr/lib/erlang/lib/kernel-*)))
+ERLANG_KERNEL_DIR = usr/lib/erlang/lib/kernel-$(ERLANG_KERNEL_VSN)
+
+# Install to staging to make erlang-history available to
+# Erlang release generation tools
+define ERLANG_HISTORY_INSTALL_STAGING_CMDS
+	cd $(@D) && $(HOST_DIR)/usr/bin/escript install.escript \
+	    $(STAGING_DIR)/$(ERLANG_KERNEL_DIR)/ebin $(ERLANG_KERNEL_VSN)
+endef
+
+define ERLANG_HISTORY_INSTALL_TARGET_CMDS
+	cd $(@D) && $(HOST_DIR)/usr/bin/escript install.escript \
+	    $(TARGET_DIR)/$(ERLANG_KERNEL_DIR)/ebin $(ERLANG_KERNEL_VSN)
+endef
+
+$(eval $(generic-package))
