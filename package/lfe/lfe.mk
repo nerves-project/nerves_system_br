2656587e76f3a2e1eb5d5ef919dd81a93b37c7b0
Frank Hunleth <fhunleth@troodon-software.com>
2015-02-15 12:21:17 -0500

    lfe: update to use new rebar infrastructure

--- package/lfe/lfe.mk
+++ package/lfe/lfe.mk
@@ -9,48 +9,22 @@ LFE_SITE = $(call github,rvirding,lfe,v$(LFE_VERSION))
 LFE_LICENSE = Apache-2.0
 LFE_LICENSE_FILES = LICENSE
 LFE_INSTALL_STAGING = YES
-LFE_INSTALL_TARGET = NO
 
-# LFE contains a platform-specific shared library, so the usual
-# configuration will have one built for the host and one for
-# the target. Project that use LFE should be careful to use the
-# host version to run lfe scripts and the target beam files when
-# making releases.
-LFE_DEPENDENCIES = host-erlang erlang host-lfe
-HOST_LFE_DEPENDENCIES = host-erlang host-erlang-rebar
+LFE_PROGRAMS = lfe lfec lfescript
 
-LFE_INSTALL_DIR_OFFSET = usr/lib/erlang/lib/lfe-$(LFE_VERSION)
-
-define LFE_BUILD_CMDS
-	(cd $(@D) && $(REBAR) compile)
-endef
-
-# Install the target LFE to staging. Projects that depend on LFE
-# should reference this staging directory when making releases.
-define LFE_INSTALL_STAGING_CMDS
-	$(INSTALL) -d $(STAGING_DIR)/$(LFE_INSTALL_DIR_OFFSET)
-	for dir in bin ebin priv ; do \
-	    cp -r $(@D)/$${dir} $(STAGING_DIR)/$(LFE_INSTALL_DIR_OFFSET) ; \
+define TARGET_LFE_INSTALL_PROGRAMS
+	for bin in $(LFE_PROGRAMS) ; do \
+		ln -sf $(TARGET_DIR)/usr/lib/erlang/lib/lfe-$(LFE_VERSION)/bin/$${bin} $(HOST_DIR)/usr/bin ; \
 	done
 endef
+HOST_LFE_POST_TARGET_INSTALL_HOOKS += TARGET_LFE_INSTALL_PROGRAMS
 
-define HOST_LFE_BUILD_CMDS
-	$(HOST_MAKE_ENV) $(MAKE) -C $(@D)
-endef
-
-# The LFE Makefile installs symlinks to its binaries in /usr/bin.
-# This is not convenient for packaging tools that need to reference
-# LFE's .beam files, so install everything to /usr/lib/lfe
-define HOST_LFE_INSTALL_CMDS
-	$(INSTALL) -d $(HOST_DIR)/$(LFE_INSTALL_DIR_OFFSET)
-	for dir in bin ebin priv ; do \
-	    cp -r $(@D)/$${dir} $(HOST_DIR)/$(LFE_INSTALL_DIR_OFFSET) ; \
-	done
-	$(INSTALL) -d $(HOST_DIR)/usr/lib/lfe
-	for bin in lfe lfec lfescript ; do \
-		ln -sf $(HOST_DIR)/$(LFE_INSTALL_DIR_OFFSET)/bin/$${bin} $(HOST_DIR)/usr/bin ; \
+define HOST_LFE_INSTALL_PROGRAMS
+	for bin in $(LFE_PROGRAMS) ; do \
+		ln -sf $(HOST_DIR)/usr/lib/erlang/lib/lfe-$(LFE_VERSION)/bin/$${bin} $(HOST_DIR)/usr/bin ; \
 	done
 endef
+HOST_LFE_POST_INSTALL_HOOKS += HOST_LFE_INSTALL_PROGRAMS
 
-$(eval $(generic-package))
-$(eval $(host-generic-package))
+$(eval $(rebar-package))
+$(eval $(host-rebar-package))
