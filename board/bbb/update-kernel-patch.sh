980584a710c2a1c1117ae732f0cdb59bea124bf8
Frank Hunleth <fhunleth@troodon-software.com>
2015-09-12 14:29:32 -0400

    bbb: update kernel patches to latest
    This also updates the update-kernel-patch.sh script to support binary
    diffs properly. This was needed for the 3.14 kernel, but is also used
    for 3.8.

--- board/bbb/update-kernel-patch.sh
+++ board/bbb/update-kernel-patch.sh
@@ -6,26 +6,58 @@ set -e
 # of the script for setting the versions.
 
 update_kernel_patch() {
-  local KERNEL_VERSION=$1
-  local OUTPUT_PATCH=$2
+  local PATCH_VERSION=$1
+  local KERNEL_VERSION=$2
+  local OUTPUT_PATCH=$3
+
+  local ORIGINAL_DIFF=patch-$PATCH_VERSION.diff
+  local ORIGINAL_DIFF_GZ=$ORIGINAL_DIFF.gz
+  local KERNEL_TARBALL=linux-$KERNEL_VERSION.tar.xz
+
+  # Work in a temporary directory
+  rm -fr work
+  mkdir -p work
+  cd work
+
+  echo "Downloading files..."
 
   # Download the master patch file for the RCN kernel
-  local ORIGINAL_DIFF_GZ=patch-$KERNEL_VERSION.diff.gz
-  rm -f $ORIGINAL_DIFF_GZ
-  wget http://rcn-ee.net/deb/sid-armhf/v$KERNEL_VERSION/$ORIGINAL_DIFF_GZ
+  wget http://rcn-ee.net/deb/sid-armhf/v$PATCH_VERSION/$ORIGINAL_DIFF_GZ
 
-  # Expand the compressed diff and filter it through filterdiff.
-  # Since buildroot uses patch instead of "git am" to apply diffs,
-  # the diff file needs to be compatible. The git diff from RCN
-  # is close, but contains one GIT binary diff that causes buildroot
-  # to fail.
-  gunzip -c $ORIGINAL_DIFF_GZ | filterdiff > $OUTPUT_PATCH
+  # Download the kernel that it will patch
+  wget https://www.kernel.org/pub/linux/kernel/v3.x/$KERNEL_TARBALL
+
+  # Extract the kernel twice so that the patch can be compared
+  echo "Extracting..."
+  tar -x -f $KERNEL_TARBALL && mv linux-$KERNEL_VERSION a
+  tar -x -f $KERNEL_TARBALL && mv linux-$KERNEL_VERSION b
+
+  # Expand the "git" style patch
+  gunzip $ORIGINAL_DIFF_GZ
+
+  # Make a git repo and apply the "git" style patch
+  echo "Creating temporary git repo to extract RCN patch..."
+  cd b
+  git init
+  git add .
+  git commit -q -m "Initial commit"
+  git apply ../$ORIGINAL_DIFF
+  rm -fr .git
+
+  # Now create a regular "diff" patch
+  echo "Creating patch..."
+  cd ..
+  diff -Naur --no-dereference a b > ../$OUTPUT_PATCH || :
 
   # Clean up
-  rm -f $ORIGINAL_DIFF_GZ
+  echo "Cleaning up..."
+  cd ..
+  rm -fr work
+
+  return 0
 }
 
-update_kernel_patch 3.8.13-bone70 rcn-linux-kernel-3.8.patch
-update_kernel_patch 3.14.17-bone8 rcn-linux-kernel-3.14.patch
+update_kernel_patch 3.8.13-bone73 3.8.13 rcn-linux-kernel-3.8.patch
+update_kernel_patch 3.14.51-ti-r74 3.14.51 rcn-linux-kernel-3.14.patch
 
-echo Updated patches. Now rebuild the linux kernel.
+echo "Updated patches. Now rebuild the linux kernel."
