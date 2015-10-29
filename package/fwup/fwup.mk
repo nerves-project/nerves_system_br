02ced632d1a5c4b41f8108f809c374cdad4c82c9
Frank Hunleth <fhunleth@troodon-software.com>
2015-10-28 21:22:37 -0400

    fwup: bump to v0.4.2
     * add libsodium patch
     * fix legal-info

--- package/fwup/fwup.mk
+++ package/fwup/fwup.mk
@@ -4,12 +4,12 @@
 #
 #############################################################
 
-FWUP_VERSION = v0.2.0
+FWUP_VERSION = v0.4.2
 FWUP_SITE = $(call github,fhunleth,fwup,$(FWUP_VERSION))
 FWUP_LICENSE = Apache-2.0
-FWUP_LICENSE_FILES = COPYING
+FWUP_LICENSE_FILES = LICENSE
 FWUP_AUTORECONF = YES
-FWUP_DEPENDENCIES = libconfuse libarchive
+FWUP_DEPENDENCIES = libconfuse libarchive libsodium
 
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
