6000c6b05df5152fc148f60784499420b4f661dc
Will Penington <will@willpenington.com>
2015-11-02 16:55:17 +1100

    Make nerves-env.sh compatible with zsh and others
    Update nerves-env.sh to call itself under Bash if used in another shell
    to get the correct file location.
    
    Tested in Bash and zsh calling directly, through a symlink, an alias and
    PATH.
    
    FAH: Modified to work on OSX too.

--- nerves-env.sh
+++ nerves-env.sh
@@ -3,25 +3,43 @@
 # Source this script to setup your environment to cross-compile
 # and build Erlang apps for this Nerves build.
 
-if [ "$SHELL" != "/bin/bash" ]; then
-    echo ERROR: This script currently only works from bash.
-    exit 1
+# If the script is called with the -get-nerves-root flag it just returns the
+# Nerves SDK directory. This is so that other shells can execute the script
+# without needing to implement the equivalent of $BASH_SOURCE for every shell.
+for arg in $*
+do
+    if [ $arg = "-get-nerves-root" ];
+    then
+        echo $(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
+        exit 0
+    fi
+done
+
+if [ "$BASH_SOURCE" = "" ]; then
+    GET_NR_COMMAND="$0 $@ -get-nerves-root"
+    NERVES_ROOT=$(bash -c "$GET_NR_COMMAND")
+else
+    # Mac note: Don't use "readlink -f" below since it doesn't work on BSD
+    NERVES_ROOT=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 fi
 
-if [ "$0" != "bash" -a "$0" != "-bash" -a "$0" != "/bin/bash" ]; then
-    echo ERROR: This scripted should be sourced from bash:
-    echo
-    echo source $BASH_SOURCE
-    echo
-    return 1
-    exit 1
+# Detect if this script has been run directly rather than sourced, since
+# that won't work.
+if [[ "$SHELL" = "/bin/bash" ]]; then
+    if [ "$0" != "bash" -a "$0" != "-bash" -a "$0" != "/bin/bash" ]; then
+        echo ERROR: This scripted should be sourced from bash:
+        echo
+        echo source $BASH_SOURCE
+        echo
+        exit 1
+    fi
+#elif [[ "$SHELL" = "/bin/zsh" ]]; then
+# TODO: Figure out how to detect this error from other shells.
 fi
 
 # We need to find the path to this script file, since all of our
 # data files are relative to it.
 #
-# Mac note: Don't use "readlink -f" below since it doesn't work on BSD
-NERVES_ROOT=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 NERVES_DEFCONFIG=`grep BR2_DEFCONFIG= $NERVES_ROOT/buildroot/.config | sed -e 's/.*"\(.*\)"/\1/'`
 
 source $NERVES_ROOT/scripts/nerves-env-helper.sh $NERVES_ROOT
