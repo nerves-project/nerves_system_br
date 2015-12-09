9c7ce089eda1f0f6b3879399254c028ef72f4487
Frank Hunleth <fhunleth@troodon-software.com>
2015-12-08 23:57:34 -0500

    Improve the naming of the build artifacts
    This adds the config used to the name and for ci builds, it adds the
    branch or tag.

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,5 +1,5 @@
-# Much of this was copied from the Buildroot .travis.yml. See
-# https://github.com/buildroot/buildroot-defconfig-testing/blob/master/.travis.yml
+#!/bin/bash
+
 export DEFCONFIG_NAME="nerves_${NERVES_CONFIG}_defconfig"
 export LD_LIBRARY_PATH=
 
@@ -7,16 +7,23 @@ export LD_LIBRARY_PATH=
 make --silent $DEFCONFIG_NAME
 
 # Build the SDK
-while true ; do echo "Still building" ; sleep 60 ; done &
-stupidpid=$!
-make > >(tee build.log | grep '>>>') 2>&1
-kill ${stupidpid}
-echo 'Display end of log'
-tail -1000 build.log
 
-# Create artifacts
-make system
+# The watchdog logic to make Travis happy is courtesy of the Buildroot project. See
+# https://github.com/buildroot/buildroot-defconfig-testing/blob/master/.travis.yml
+while true ; do echo "Still building" ; sleep 60 ; done &
+watchdogpid=$!
+make > >(tee build.log | grep '>>>') 2>&1
+kill ${watchdogpid}
+echo 'Display end of log'
+tail -500 build.log
+
+# Create a system image
+BRANCH_OR_TAG=$(git describe --exact-match 2>/dev/null || git rev-parse --abbrev-ref HEAD)
+SYSTEM_ARCHIVE_NAME=nerves-system-$NERVES_CONFIG_NAME-$BRANCH_OR_TAG
+scripts/mksystem.sh $SYSTEM_ARCHIVE_NAME
+
+# Save the artifacts
 rm -fr artifacts
 mkdir artifacts
-cp system.tar.gz artifacts
-cp buildroot/output/images/*.fw artifacts
+cp $SYSTEM_ARCHIVE_NAME.tar.gz artifacts
+cp buildroot/output/images/*.fw artifacts/$NERVES_CONFIG_NAME-$BRANCH_OR_TAG.fw # only one .fw file in images
--- scripts/mksystem.sh
+++ scripts/mksystem.sh
@@ -8,13 +8,39 @@
 #
 set -e
 
+# The first parameter can be the output archive name. If it's not set
+# we'll create one.
+ARCHIVE_NAME=$1
+
 NERVES_ROOT=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)/..
+NERVES_DEFCONFIG=$(grep BR2_DEFCONFIG= $NERVES_ROOT/buildroot/.config | sed -e 's/BR2_DEFCONFIG=".*\/\(.*\)"/\1/')
+
+# Strip the _defconfig off the configuration name for the image name
+NERVES_CONFIG_NAME=$(echo $NERVES_DEFCONFIG | sed -e 's/\(.*\)_defconfig$/\1/')
 
 WORK_DIR=$NERVES_ROOT/tmp-system
-ARCHIVE_NAME=system
+[ -z $ARCHIVE_NAME ] && ARCHIVE_NAME=nerves-system-$NERVES_CONFIG_NAME
 
 rm -fr $WORK_DIR
 mkdir -p $WORK_DIR/$ARCHIVE_NAME
+
+# Save a tag to the archive in case we need it for debug
+git describe --dirty >$WORK_DIR/$ARCHIVE_NAME/nerves-system.tag
+
+# Add some help text for the curious
+cat << EOT > $WORK_DIR/$ARCHIVE_NAME/README.md
+# Nerves system image
+
+This is an automatically generated archive created by \`nerves-sdk\`. It is
+useful for building embedded Elixir projects without worrying too much
+about the cross-compiler and Linux parts. See http://nerves-project.org
+for more information.
+
+## Build information
+
+Configuration: $NERVES_DEFCONFIG
+git: $(git describe --always --dirty)
+EOT
 
 # Copy common nerves shell scripts over
 cp $NERVES_ROOT/nerves-env.sh $WORK_DIR/$ARCHIVE_NAME
