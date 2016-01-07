dc6a88ec550754994b348bec0314bd2a93fdff7d
Frank Hunleth <fhunleth@troodon-software.com>
2016-01-07 11:27:51 -0500

    Remove line to copy uEnv.txt
    The defaults are built into the script now.

--- board/bbb/post-createfs.sh
+++ board/bbb/post-createfs.sh
@@ -10,7 +10,6 @@ BASE_FW_NAME=nerves-bbb-base
 
 # Create/copy u-boot files to the images directory
 $HOSTDIR/usr/bin/mkimage -A arm -O linux -T script -C none -a 0 -e 0 -n "nerves boot script" -d $NERVES_SYSTEM/board/bbb/uboot-script.cmd $IMAGEDIR/boot.scr
-cp $NERVES_SYSTEM/board/bbb/uEnv.txt $IMAGEDIR
 
 # Run the common post-image processing for nerves
 $NERVES_SYSTEM/board/nerves-common/post-createfs.sh $TARGETDIR $FWUP_CONFIG $BASE_FW_NAME
