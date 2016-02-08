76929d66c119eeb29b2ed62200ba1c358859e1b0
Frank Hunleth <fhunleth@troodon-software.com>
2016-02-07 21:43:34 -0500

    fwup: bump to version 0.6.0
    This fixes the AM335x bootloader issue with making FAT filesystems.

--- package/fwup/fwup.mk
+++ package/fwup/fwup.mk
@@ -4,12 +4,15 @@
 #
 #############################################################
 
-FWUP_VERSION = 0.5.2
+FWUP_VERSION = 0.6.0
 # Use the official release tarball
 FWUP_SITE = https://github.com/fhunleth/fwup/releases/download/v$(FWUP_VERSION)
 FWUP_LICENSE = Apache-2.0
 FWUP_LICENSE_FILES = LICENSE
 FWUP_DEPENDENCIES = libconfuse libarchive libsodium
+
+# help2man doesn't work when cross compiling
+FWUP_CONF_ENV = ac_cv_path_HELP2MAN=''
 
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
