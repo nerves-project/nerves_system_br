00d53879f47b9ff107cb72ccdeb01e750ddced1d
Frank Hunleth <fhunleth@troodon-software.com>
2016-02-14 10:08:28 -0500

    Fix Galileo build

--- board/galileo/post-createfs.sh
+++ board/galileo/post-createfs.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-FWUP_CONFIG=$NERVES_SYSTEM/board/galileo/fwup.conf
+FWUP_CONFIG=$BR2_EXTERNAL/board/galileo/fwup.conf
 
 # Run the common post-image processing for nerves
-$NERVES_SYSTEM/board/nerves-common/post-createfs.sh $TARGET_DIR $FWUP_CONFIG
+$BR2_EXTERNAL/board/nerves-common/post-createfs.sh $TARGET_DIR $FWUP_CONFIG
