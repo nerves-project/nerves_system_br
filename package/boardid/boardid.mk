8b5e614d7911957308ba36ef5859ba588c5f24f7
Frank Hunleth <fhunleth@troodon-software.com>
2016-03-03 20:17:24 -0500

    new package: boardid
    This replaces nerves-id with a standalone program. This also works for
    the BBB.

---
+++ package/boardid/boardid.mk
@@ -0,0 +1,20 @@
+#############################################################
+#
+# boardid
+#
+#############################################################
+
+BOARDID_VERSION = v0.1.0
+BOARDID_SITE = $(call github,fhunleth,boardid,$(BOARDID_VERSION))
+BOARDID_LICENSE = Apache-2.0
+BOARDID_LICENSE_FILES = LICENSE
+
+define BOARDID_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)
+endef
+
+define BOARDID_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 755 $(@D)/boardid $(TARGET_DIR)/usr/bin/boardid
+endef
+
+$(eval $(generic-package))
