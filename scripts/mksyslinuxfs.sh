3c999f7b3e710635b5e904927d05ad36101e8f73
Frank Hunleth <fhunleth@troodon-software.com>
2016-03-24 00:16:27 -0400

    Get the ag150 and alix configs building again
    Neither is test, but they at least build now.

--- scripts/mksyslinuxfs.sh
+++ scripts/mksyslinuxfs.sh
@@ -1,24 +1,41 @@
-#!/bin/sh
+#!/bin/bash
 
 # This helper script creates a FAT file system with the syslinux special
 # files. It should be passed the filename of the image, the size in
 # 512 byte blocks, and the path to syslinux.
+#
+# Inputs:
+#   $BASE_DIR   The Buildroot output directory
+#   $1          The block offset of the boot partition
+#   $2          The size in blocks of the boot partition
 
 set -e
 
-if [ $# -lt 4 ]; then
-    echo "Usage: $0 <nerves_root> <bootpart> <bootsize> <syslinux path>"
+if [[ $# -lt 2 ]]; then
+    echo "Usage: $0 <bootpart> <bootsize>"
     exit 1
 fi
 
-NERVES_SYSTEM=$1
-BOOTPART=$2
-BOOTSIZE=$3
-SYSLINUX=$4
+BOOTPART=$1
+BOOTSIZE=$2
+
+MKFS_VFAT=$BASE_DIR/host/usr/sbin/mkfs.vfat
+if [[ ! -f $MKFS_VFAT ]]; then
+    # Use the system mkfs.vfat if the Buildroot one isn't available
+    # sudo apt-get install dosfstools
+    MKFS_VFAT=mkfs.vfat
+fi
+
+SYSLINUX=$BASE_DIR/host/usr/bin/syslinux
+if [[ ! -f $SYSLINUX ]]; then
+    # Use the system syslinux if the Buildroot one isn't available
+    # sudo apt-get install dosfstools
+    SYSLINUX=syslinux
+fi
 
 # Create the boot partition and run it through syslinux
 rm -f $BOOTPART
 dd if=/dev/zero of=$BOOTPART count=0 seek=$BOOTSIZE 2>/dev/null
-$NERVES_SYSTEM/host/usr/sbin/mkfs.vfat -F 12 -n BOOT $BOOTPART >/dev/null
+$MKFS_VFAT -F 12 -n BOOT $BOOTPART >/dev/null
 $SYSLINUX -i $BOOTPART
 
