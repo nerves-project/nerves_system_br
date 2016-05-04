23fee2d05ee356dc61cd6a9cca306cc07571a3b2
Frank Hunleth <fhunleth@troodon-software.com>
2016-05-04 17:29:45 -0400

    erlang-history: bump to support 18.3

--- package/erlang-history/erlang-history.mk
+++ package/erlang-history/erlang-history.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-ERLANG_HISTORY_VERSION = 859780a5a1c9e7130226aa9a03077ba9064c46db
+ERLANG_HISTORY_VERSION = f482e19b188eb47a28ac0bc5b62d25e975c874d5
 ERLANG_HISTORY_SITE = $(call github,ferd,erlang-history,$(ERLANG_HISTORY_VERSION))
 ERLANG_HISTORY_DEPENDENCIES = erlang host-erlang
 ERLANG_HISTORY_INSTALL_STAGING = YES
