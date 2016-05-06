5fad3091304064f2b7fa2c548c3aaf699504d8ee
Frank Hunleth <fhunleth@troodon-software.com>
2016-05-06 12:18:03 -0400

    Update BBB uboot script to support eMMC boots
    It seems like uboot changed how eMMC and SDCard slots are referenced in
    one of the version updates. This fixes the issue and supplies plenty of
    comments.

--- board/bbb/uboot-script.cmd
+++ board/bbb/uboot-script.cmd
@@ -19,12 +19,15 @@ echo "Running Nerves U-Boot script"
 # mode.
 
 # When debugging, create a uEnv.txt file in the BOOT partition
-# to override these arguments.
+# to override these arguments. For example, if you want the
+# kernel debug messages, put this in a uEnv.txt:
+#   optargs="capemgr.disable_partno=BB-BONELT-HDMI,BB-BONELT-HDMIN uio_pruss.extram_pool_sz=0x100000"
+
 env set optargs "quiet capemgr.disable_partno=BB-BONELT-HDMI,BB-BONELT-HDMIN uio_pruss.extram_pool_sz=0x100000"
 
 # Allow the user to override the kernel/erlinit arguments
 # via a "uEnv.txt" file in the FAT partition.
-if load mmc 0:1 ${loadaddr} uEnv.txt; then
+if load mmc ${mmcdev}:1 ${loadaddr} uEnv.txt; then
     echo "uEnv.txt found. Overriding environment."
     env import -t -r ${loadaddr} ${filesize}
 
@@ -36,13 +39,19 @@ if load mmc 0:1 ${loadaddr} uEnv.txt; then
 fi
 
 # Determine the boot arguments
+#
+# Note the root filesystem specification. In Linux, /dev/mmcblk0 is always
+# the boot device. In uboot, mmc 0 is the SDCard and mmc 1 is the eMMC.
+# Therefore, we hardcode root=/dev/mmcblk0p2 since we always want to mount
+# the root partition off the same device that ran u-boot and supplied
+# zImage.
 setenv bootargs console=${console} ${optargs} root=/dev/mmcblk0p2 rootfstype=squashfs ro rootwait
 
 # Load the kernel
-load mmc 0:1 ${loadaddr} zImage
+load mmc ${mmcdev}:1 ${loadaddr} zImage
 
 # Load the DT. On the BBB, fdtfile=am335x-boneblack.dtb
-load mmc 0:1 ${fdtaddr} ${fdtfile}
+load mmc ${mmcdev}:1 ${fdtaddr} ${fdtfile}
 
 # Boot!!
 bootz ${loadaddr} - ${fdtaddr}
