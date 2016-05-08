afacbe314388c7119fd00ef512f20a5174cb92b3
Justin Schneck <jschneck@mac.com>
2016-05-08 17:17:24 -0400

    added hex package info

--- mix.exs
+++ mix.exs
@@ -5,8 +5,22 @@ defmodule Nerves.System.BR do
     [app: :nerves_system_br,
      version: "0.4.1",
      elixir: "~> 1.2",
+     description: description,
+     package: package,
      compilers: [:app]
     ]
+  end
+
+  defp description do
+   """
+   Nerves System BR - Buildroot based build platform for Nerves Systems
+   """
+  end
+
+  defp package do
+   [maintainers: ["Frank Hunleth", "Justin Schneck"],
+    licenses: ["Apache 2.0"],
+    links: %{"Github" => "https://github.com/nerves-project/nerves_system_br"}]
   end
 
 end
