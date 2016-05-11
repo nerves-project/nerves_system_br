f2d1d051677879487bb8546b3d73b3180b8cde08
Frank Hunleth <fhunleth@troodon-software.com>
2016-05-11 19:08:10 -0400

    bborg-overlays: new package
    The device tree overlays were removed from the kernel patches in the 4.x
    kernels. This package compiles and installs them.

---
+++ package/bborg-overlays/bborg-overlays.mk
@@ -0,0 +1,25 @@
+#############################################################
+#
+# bborg-overlays
+#
+#############################################################
+
+BBORG_OVERLAYS_VERSION = 4d19f3aa27be100fafb1eee9ed0b01a6079ea2af
+BBORG_OVERLAYS_SITE = $(call github,beagleboard,bb.org-overlays,$(BBORG_OVERLAYS_VERSION))
+BBORG_OVERLAYS_LICENSE = GPLv2
+BBORG_OVERLAYS_DEPENDENCIES = host-dtc
+
+# See https://github.com/beagleboard/bb.org-overlays/blob/master/Makefile
+# for preprocessing logic before invoking dtc
+define BBORG_OVERLAYS_BUILD_CMDS
+	for filename in $(@D)/src/arm/*.dts; do \
+	    $(CPP) -I$(@D)/include -I$(@D)/src/arm -nostdinc -undef -D__DTS__ -x assembler-with-cpp $$filename | \
+	      $(HOST_DIR)/usr/bin/dtc -@ -I dts -O dtb -b 0 -o $${filename%.dts}.dtbo || exit 1; \
+	done
+endef
+
+define BBORG_OVERLAYS_INSTALL_TARGET_CMDS
+	cp $(@D)/src/arm/*.dtbo $(TARGET_DIR)/lib/firmware
+endef
+
+$(eval $(generic-package))
