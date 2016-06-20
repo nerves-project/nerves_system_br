3f2c7aaf53c439e8c90a51fbac5f344336dafaee
Frank Hunleth <fhunleth@troodon-software.com>
2016-06-20 13:29:41 -0400

    fwup: bump to v0.8.0
    This also aligns the files more closely to what was just committed in
    Buildroot.

--- package/fwup/fwup.mk
+++ package/fwup/fwup.mk
@@ -4,15 +4,13 @@
 #
 #############################################################
 
-FWUP_VERSION = 0.7.0
-# Use the official release tarball
-FWUP_SITE = https://github.com/fhunleth/fwup/releases/download/v$(FWUP_VERSION)
+FWUP_VERSION = v0.8.0
+FWUP_SITE = $(call github,fhunleth,fwup,$(FWUP_VERSION))
 FWUP_LICENSE = Apache-2.0
 FWUP_LICENSE_FILES = LICENSE
 FWUP_DEPENDENCIES = libconfuse libarchive libsodium
-
-# help2man doesn't work when cross compiling
-FWUP_CONF_ENV = ac_cv_path_HELP2MAN=''
+FWUP_AUTORECONF = YES
+FWUP_CONF_ENV = ac_cv_path_HELP2MAN=
 
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
