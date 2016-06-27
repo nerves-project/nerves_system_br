2504cda9c32e25f6bd377230b04091813021a3e0
Frank Hunleth <fhunleth@troodon-software.com>
2016-06-27 17:03:22 -0400

    Move EV3 files from board to nerves_system_ev3

--- board/lego-ev3/post-createfs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -e
-
-FWUP_CONFIG=$BR2_EXTERNAL/board/lego-ev3/fwup.conf
-
-# Run the common post-image processing for nerves
-$BR2_EXTERNAL/board/nerves-common/post-createfs.sh $TARGET_DIR $FWUP_CONFIG
