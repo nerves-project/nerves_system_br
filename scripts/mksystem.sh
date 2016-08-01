0fe24ee5b37a3a0fb75e6799f9a50ad9d9502428
Frank Hunleth <fhunleth@troodon-software.com>
2016-08-01 19:16:57 -0400

    Remove .img file from system image
    The previous version assumed that the system image started with
    "nerves-", but once we changed that, the raw image file started getting
    included.

--- scripts/mksystem.sh
+++ scripts/mksystem.sh
@@ -81,7 +81,7 @@ cp -R $BASE_DIR/staging/* $WORK_DIR/$ARCHIVE_NAME/staging
 
 # Clean up extra files that were copied over and aren't needed
 rm -f $WORK_DIR/$ARCHIVE_NAME/images/*.fw
-rm -f $WORK_DIR/$ARCHIVE_NAME/images/nerves-*.img
+rm -f $WORK_DIR/$ARCHIVE_NAME/images/$ARCHIVE_NAME.img
 
 # The --format=ustar makes it possible for erl_tar to extract these archives
 tar c -z -f $BASE_DIR/$ARCHIVE_NAME.tar.gz -C $WORK_DIR --format=ustar $ARCHIVE_NAME
