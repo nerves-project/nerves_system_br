c90f3a120e031f8d0ebbbcf80a1beb0bb81d1538
Frank Hunleth <fhunleth@troodon-software.com>
2016-10-14 08:55:12 -0400

    Move Galileo files to nerves_system_galileo

--- board/galileo/post-build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -e
-
-# Copy over the grub config to the EFI directory
-cp $BR2_EXTERNAL/board/galileo/grub.cfg $BINARIES_DIR/efi-part/EFI/BOOT/grub.cfg
-
-# Run the common post-build processing for nerves
-$BR2_EXTERNAL/board/nerves-common/post-build.sh $TARGET_DIR
--- board/galileo/post-createfs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -e
-
-FWUP_CONFIG=$BR2_EXTERNAL/board/galileo/fwup.conf
-
-# Run the common post-image processing for nerves
-$BR2_EXTERNAL/board/nerves-common/post-createfs.sh $TARGET_DIR $FWUP_CONFIG
