758ce9eda4a57899288c5882a3d3a928432c13ae
Justin Schneck <jschneck@mac.com>
2016-11-21 21:52:58 -0500

    include version in mix package

--- mix.exs
+++ mix.exs
@@ -24,7 +24,7 @@ defmodule Nerves.System.BR.Mixfile do
   defp package do
    [maintainers: ["Frank Hunleth", "Justin Schneck"],
     files: ["board", "package", "patches", "scripts", "Config.in", "create-build.sh", "external.mk", "LICENSE", "mix.exs", "nerves_env.exs",
-    "nerves-env.sh", "nerves.exs", "nerves.mk", "README.md"],
+    "nerves-env.sh", "nerves.exs", "nerves.mk", "README.md", "VERSION"],
     licenses: ["Apache 2.0", "GPLv2"],
     links: %{"Github" => "https://github.com/nerves-project/nerves_system_br"}]
   end
