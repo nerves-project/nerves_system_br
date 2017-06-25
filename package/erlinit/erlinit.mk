acf467ac7394c79f242700e93285b73faf3340a1
Frank Hunleth <fhunleth@troodon-software.com>
2017-06-25 17:09:06 -0400

    erlinit: bump to v1.1.1
    This fixes an issue where orphaned zombie processes would stack up. It
    wasn't a big deal unless your application liked to orphan zombie
    processes. Nerves.Network did this at one point.

--- package/erlinit/erlinit.mk
+++ package/erlinit/erlinit.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-ERLINIT_VERSION = v1.1.0
+ERLINIT_VERSION = v1.1.1
 ERLINIT_SITE = $(call github,nerves-project,erlinit,$(ERLINIT_VERSION))
 ERLINIT_LICENSE = MIT
 ERLINIT_LICENSE_FILES = LICENSE
