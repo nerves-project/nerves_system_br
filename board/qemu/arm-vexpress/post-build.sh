8234a43499968468b461dda45da180646bc26d4d
Frank Hunleth <fhunleth@troodon-software.com>
2017-07-12 09:41:34 -0400

    Move qemu configuration to nerves_system_qemu_arm

--- board/qemu/arm-vexpress/post-build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-set -e
-
-# Run the common post-build processing for nerves
-$BR2_EXTERNAL_NERVES_PATH/board/nerves-common/post-build.sh $1
--- board/qemu/arm-vexpress/post-createfs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -e
-
-FWUP_CONFIG=$BR2_EXTERNAL_NERVES_PATH/board/qemu/arm-vexpress/fwup.conf
-
-# Run the common post-image processing for nerves
-$BR2_EXTERNAL_NERVES_PATH/board/nerves-common/post-createfs.sh $TARGET_DIR $FWUP_CONFIG
