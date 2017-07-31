4ce5fdc6a923b03a3ab4dc4cd02838b940311de2
Justin Schneck <jschneck@mac.com>
2017-07-31 15:04:27 -0400

    String.strip was deprecated in favor of String.trim

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Nerves.System.BR.Mixfile do
 
 @version Path.join(__DIR__, "VERSION")
          |> File.read!
-         |> String.strip
+         |> String.trim
 
   def project do
     [app: :nerves_system_br,
