73ea2f80825b717fedb0956a7c690271bb57b04b
Frank Hunleth <fhunleth@troodon-software.com>
2017-10-06 15:13:56 -0400

    nbtty: new package
    This is a prototype app for preventing iex hangs w/ gadget mode.  It
    also supports figuring out the terminal size over a serial port.

---
+++ package/nbtty/nbtty.mk
@@ -0,0 +1,18 @@
+################################################################################
+#
+# nbtty
+#
+################################################################################
+
+NBTTY_VERSION = v0.3.0
+NBTTY_SITE = $(call github,fhunleth,nbtty,$(NBTTY_VERSION))
+NBTTY_LICENSE = GPL-2.0+
+NBTTY_LICENSE_FILES = COPYING
+NBTTY_AUTORECONF = YES
+
+# The Makefile does not have an install target.
+define NBTTY_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/nbtty $(TARGET_DIR)/usr/bin/nbtty
+endef
+
+$(eval $(autotools-package))
