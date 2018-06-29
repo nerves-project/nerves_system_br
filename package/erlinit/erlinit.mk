ba89dd5adc53c7853636e1617e4687ee15a1770a
Frank Hunleth <fhunleth@troodon-software.com>
2018-06-29 16:18:57 +0000

    erlinit: bump to v1.4.2
    This pulls in an update to the filesystem mount calls that fixes
    a parse error from Docker.

--- package/erlinit/erlinit.mk
+++ package/erlinit/erlinit.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-ERLINIT_VERSION = v1.4.1
+ERLINIT_VERSION = v1.4.2
 ERLINIT_SITE = $(call github,nerves-project,erlinit,$(ERLINIT_VERSION))
 ERLINIT_LICENSE = MIT
 ERLINIT_LICENSE_FILES = LICENSE
