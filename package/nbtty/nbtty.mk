52d95eae7492d9ce72bbc5053c4517dcba6b2b4f
Frank Hunleth <fhunleth@troodon-software.com>
2018-11-03 22:16:09 -0400

    nbtty: bump to v0.4.1
    This adds an option to disable output to the tty until the user presses
    the enter key. This reduces I/O when no one is paying attention and can
    remove the random output on the USB gadget tty on initial connect.

--- package/nbtty/nbtty.mk
+++ package/nbtty/nbtty.mk
@@ -4,15 +4,10 @@
 #
 ################################################################################
 
-NBTTY_VERSION = v0.4.0
+NBTTY_VERSION = v0.4.1
 NBTTY_SITE = $(call github,fhunleth,nbtty,$(NBTTY_VERSION))
 NBTTY_LICENSE = GPL-2.0+
 NBTTY_LICENSE_FILES = COPYING
 NBTTY_AUTORECONF = YES
-
-# The Makefile does not have an install target.
-define NBTTY_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/nbtty $(TARGET_DIR)/usr/bin/nbtty
-endef
 
 $(eval $(autotools-package))
