af26d9d29b2a69bfd7f549d810365b6931cd90bf
Matt Ludwigs <mattludwigs@gmail.com>
2019-09-07 13:53:39 -0700

    Fix shellcheck ci-deploy warnings

--- scripts/ci-deploy.sh
+++ scripts/ci-deploy.sh
@@ -38,10 +38,10 @@ fi
 rm -fr artifacts
 mkdir -p artifacts/$ARTIFACT_SUBDIR
 
-WORK_DIR=_build/*/nerves/system
+WORK_DIR="_build/*/nerves/system"
 if [ ! -d "$WORK_DIR" ]; then
-  WORK_DIR=.nerves/artifacts/*
+  WORK_DIR=".nerves/artifacts/*"
 fi
 
-cp $WORK_DIR/${CI_SYSTEM_NAME}.tar.gz artifacts/$ARTIFACT_SUBDIR/${CI_SYSTEM_NAME}-$BRANCH_OR_TAG.tar.gz
-cp $WORK_DIR/images/${CI_SYSTEM_NAME}.fw artifacts/$ARTIFACT_SUBDIR/${CI_SYSTEM_NAME}-$BRANCH_OR_TAG.fw # only one .fw file in images
+cp "$WORK_DIR/${CI_SYSTEM_NAME}.tar.gz" "artifacts/$ARTIFACT_SUBDIR/${CI_SYSTEM_NAME}-$BRANCH_OR_TAG.tar.gz"
+cp "$WORK_DIR/images/${CI_SYSTEM_NAME}.fw" "artifacts/$ARTIFACT_SUBDIR/${CI_SYSTEM_NAME}-$BRANCH_OR_TAG.fw" # only one .fw file in images
