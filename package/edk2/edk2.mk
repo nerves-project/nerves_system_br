a000ba5ef6e9c1a29c9bbb5e576b812498c9dbb1
Justin Schneck <jschneck@mac.com>
2020-01-18 08:43:49 -0500

    edk2: bump to edk2-stable201911

--- package/edk2/edk2.mk
+++ package/edk2/edk2.mk
@@ -4,13 +4,20 @@
 #
 #############################################################
 
-EDK2_VERSION = 315d9d08fd77db1024ccc5307823da8aaed85e2f
-EDK2_SITE = $(call github,tianocore,edk2,$(EDK2_VERSION))
+EDK2_VERSION = edk2-stable201911
+EDK2_SITE = https://github.com/tianocore/edk2/releases/download/$(EDK2_VERSION)
+EDK2_SOURCE = ShellBinPkg.zip
 EDK2_INSTALL_TARGET = NO
 EDK2_INSTALL_IMAGES = YES
 
+define EDK2_EXTRACT_CMDS
+        $(UNZIP) -d $(@D) $(EDK2_DL_DIR)/$(EDK2_SOURCE)
+        mv $(@D)/ShellBinPkg/* $(@D)
+        $(RM) -r $(@D)/ShellBinPkg
+endef
+
 define EDK2_INSTALL_IMAGES_CMDS
-	cp $(@D)/ShellBinPkg/UefiShell/X64/Shell.efi $(BINARIES_DIR)
+	cp $(@D)/MinUefiShell/X64/Shell.efi $(BINARIES_DIR)/efi-part
 endef
 
 $(eval $(generic-package))
