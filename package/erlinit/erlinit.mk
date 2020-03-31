a53ecfc1f70d9bc7321e61f46526d36c8692455b
Frank Hunleth <fhunleth@troodon-software.com>
2020-03-31 08:47:26 -0400

    erlinit: bump to v1.6.1
    This update fixes an issue with rootdisk symlink creation on a few
    platforms. This did not affect any of the officially supported Nerves
    systems.

--- package/erlinit/erlinit.mk
+++ package/erlinit/erlinit.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-ERLINIT_VERSION = v1.6.0
+ERLINIT_VERSION = v1.6.1
 ERLINIT_SITE = $(call github,nerves-project,erlinit,$(ERLINIT_VERSION))
 ERLINIT_LICENSE = MIT
 ERLINIT_LICENSE_FILES = LICENSE
