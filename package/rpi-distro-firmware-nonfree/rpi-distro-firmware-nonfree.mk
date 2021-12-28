140e8a5cc1dbc36e7e51866f7d536970d87ea182
Frank Hunleth <fhunleth@troodon-software.com>
2021-12-28 00:45:47 -0500

    rpi-distro/firmware-nonfree: bump version
    There have been quite a few firmware updates since the previous update
    at the beginning of 2021.
    
    See https://github.com/RPi-Distro/firmware-nonfree/blob/bullseye/debian/changelog

--- package/rpi-distro-firmware-nonfree/rpi-distro-firmware-nonfree.mk
+++ package/rpi-distro-firmware-nonfree/rpi-distro-firmware-nonfree.mk
@@ -4,13 +4,13 @@
 #
 ################################################################################
 
-RPI_DISTRO_FIRMWARE_NONFREE_VERSION = 83938f78ca2d5a0ffe0c223bb96d72ccc7b71ca5 # 1:20190114-1+rpt11 release
+RPI_DISTRO_FIRMWARE_NONFREE_VERSION = 99d5c588e95ec9c9b86d7e88d3cf85b4f729d2bc # 20210315-3+rpt4 release
 RPI_DISTRO_FIRMWARE_NONFREE_SITE = $(call github,RPi-Distro,firmware-nonfree,$(RPI_DISTRO_FIRMWARE_NONFREE_VERSION))
 RPI_DISTRO_FIRMWARE_NONFREE_LICENSE_FILES = LICENCE.broadcom_bcm43xx LICENCE.cypress
 
 define RPI_DISTRO_FIRMWARE_NONFREE_INSTALL_TARGET_CMDS
 	$(INSTALL) -d $(TARGET_DIR)/lib/firmware/brcm
-	for f in  $(@D)/brcm/*; do \
+	for f in  $(@D)/debian/config/brcm80211/brcm/*; do \
 		$(INSTALL) -D -m 0644 "$${f}" "$(TARGET_DIR)/lib/firmware/brcm/$${f##*/}" || exit 1; \
 	done
 endef
