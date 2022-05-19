c5cc21d5a1772618fd710ede06ccb0c636482a99
Frank Hunleth <fhunleth@troodon-software.com>
2022-05-19 03:41:26 -0400

    Add toolchain suffix to echo-gcc-args
    Buildroot 2022.2 started using a external suffix to toolchain commands.
    This fixes a build error on x86_64-based systems.

--- package/nerves-config/nerves-config.mk
+++ package/nerves-config/nerves-config.mk
@@ -20,6 +20,7 @@ ROOTFS_SQUASHFS_ARGS += -e srv
 
 define NERVES_CONFIG_INSTALL_TARGET_CMDS
 	cp -f $(BR2_EXTERNAL)/package/nerves-config/echo-gcc-args $(HOST_DIR)/opt/ext-toolchain/bin/echo-gcc-args
+	cp -f $(BR2_EXTERNAL)/package/nerves-config/echo-gcc-args $(HOST_DIR)/opt/ext-toolchain/bin/echo-gcc-args$(TOOLCHAIN_EXTERNAL_SUFFIX)
 	ln -sf toolchain-wrapper $(HOST_DIR)/bin/echo-gcc-args
 	$(HOST_DIR)/bin/echo-gcc-args > $(BINARIES_DIR)/buildroot-gcc-args
 endef
