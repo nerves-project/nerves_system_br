ac862edffdeb0234df4caeede18e315a974b539b
Cocoa <r.xu.1@research.gla.ac.uk>
2022-06-20 15:04:30 +0100

    set system processor

--- nerves-env.cmake
+++ nerves-env.cmake
@@ -4,6 +4,9 @@ set(CMAKE_SYSTEM_NAME "Linux")
 # set nerves sysroot
 set(CMAKE_FIND_ROOT_PATH "$ENV{NERVES_SDK_SYSROOT}")
 
+# set system processor
+set(CMAKE_SYSTEM_PROCESSOR "$ENV{TARGET_ARCH}")
+
 # adjust the default behavior of the find commands:
 # search headers and libraries in the target environment
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
