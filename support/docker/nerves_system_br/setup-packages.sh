2fb14b6811f315ffa81d1529d449f0175279aed4
Digit <aaron.shea@smartrent.com>
2022-07-19 09:26:42 -0700

    enable the Dockerfile to be built on both x86_64 and aarch64 host systems

---
+++ support/docker/nerves_system_br/setup-packages.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+if [[ $BUILD_ARCH =~ "aarch64" ]] ; then
+    dpkg --add-architecture 'i386'
+    sed 's/^deb http/deb [arch=arm64] http/' -i '/etc/apt/sources.list'
+
+    touch /etc/apt/sources.list.d/i386.list
+    echo "deb [arch=i386] http://security.ubuntu.com/ubuntu/ jammy-security main restricted universe multiverse" >> /etc/apt/sources.list.d/i386.list
+    echo "deb [arch=i386] http://archive.ubuntu.com/ubuntu/ jammy           main restricted universe multiverse" >> /etc/apt/sources.list.d/i386.list
+    echo "deb [arch=i386] http://archive.ubuntu.com/ubuntu/ jammy-updates   main restricted universe multiverse" >> /etc/apt/sources.list.d/i386.list
+    echo "deb [arch=i386] http://archive.ubuntu.com/ubuntu/ jammy-backports main restricted universe multiverse" >> /etc/apt/sources.list.d/i386.list
+else
+    dpkg --add-architecture i386
+fi
