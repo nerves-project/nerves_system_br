f2e83771a9c36599ecef8e662253648eab0526f1
Frank Hunleth <fhunleth@troodon-software.com>
2022-11-25 21:32:03 +0000

    nerves_heart: bump to v2.0.2

--- package/nerves_heart/nerves_heart.mk
+++ package/nerves_heart/nerves_heart.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-NERVES_HEART_VERSION = v2.0.1
+NERVES_HEART_VERSION = v2.0.2
 NERVES_HEART_SITE = $(call github,nerves-project,nerves_heart,$(NERVES_HEART_VERSION))
 NERVES_HEART_LICENSE = MIT
 NERVES_HEART_LICENSE_FILES = LICENSE
