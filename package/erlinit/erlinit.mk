67ef8cf70231c909698dc946c576bdb4dfce51d8
Frank Hunleth <fhunleth@troodon-software.com>
2023-11-05 16:52:36 -0500

    erlinit: bump to v1.13.0

--- package/erlinit/erlinit.mk
+++ package/erlinit/erlinit.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-ERLINIT_VERSION = v1.12.3
+ERLINIT_VERSION = v1.13.0
 ERLINIT_SITE = $(call github,nerves-project,erlinit,$(ERLINIT_VERSION))
 ERLINIT_LICENSE = MIT
 ERLINIT_LICENSE_FILES = LICENSE
