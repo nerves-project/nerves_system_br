d9f397a9047b3289795e4c40ff1b4331131fe9bf
Frank Hunleth <fhunleth@troodon-software.com>
2023-12-13 21:29:19 -0500

    rpi-distro-firmware-nonfree: bump to 1:20230210-5+rpt2
    This add RPi 5-specific links to support the BCM43455 WiFi module.
    
    It also contains about 2 years worth of changes since the previous
    version. The changelog is at
    https://github.com/RPi-Distro/firmware-nonfree/blob/88aa085bfa1a4650e1ccd88896f8343c22a24055/debian/changelog.

--- package/rpi-distro-firmware-nonfree/rpi-distro-firmware-nonfree.mk
+++ package/rpi-distro-firmware-nonfree/rpi-distro-firmware-nonfree.mk
@@ -4,12 +4,12 @@
 #
 ################################################################################
 
-RPI_DISTRO_FIRMWARE_NONFREE_VERSION = 4db8c5d80daf2220d7824cfa6052f0bb108612ea # 20210315-3+rpt5 + RPI Zero 2W update
+RPI_DISTRO_FIRMWARE_NONFREE_VERSION = 88aa085bfa1a4650e1ccd88896f8343c22a24055 # 1:20230210-5+rpt2
 RPI_DISTRO_FIRMWARE_NONFREE_SITE = $(call github,RPi-Distro,firmware-nonfree,$(RPI_DISTRO_FIRMWARE_NONFREE_VERSION))
 RPI_DISTRO_FIRMWARE_NONFREE_LICENSE_FILES = debian/config/brcm80211/copyright
 
 # Notes
-# 1. The brcmfmac43436f-sdio symlinks come from the firmware-brcm80211.postinst script
+# 1. The cyfmac43455-sdio.bin symlink comes from the firmware-brcm80211.postinst script
 # 2. The cyfmac43455-sdio-minimal firmware isn't used. It's for increasing the number
 #    of supported clients in AP mode by removing functionality.
 define RPI_DISTRO_FIRMWARE_NONFREE_INSTALL_TARGET_CMDS
@@ -18,8 +18,7 @@ define RPI_DISTRO_FIRMWARE_NONFREE_INSTALL_TARGET_CMDS
 	cp -dpfr "$(@D)/debian/config/brcm80211/cypress" "$(TARGET_DIR)/lib/firmware"
 	rm -f "$(TARGET_DIR)/lib/firmware/cypress/cyfmac43455-sdio-minimal.bin"
 	rm -f "$(TARGET_DIR)/lib/firmware/cypress/README.txt"
-	ln -sf brcmfmac43436f-sdio.bin "$(TARGET_DIR)/lib/firmware/brcm/brcmfmac43436-sdio.bin"
-	ln -sf brcmfmac43436f-sdio.txt "$(TARGET_DIR)/lib/firmware/brcm/brcmfmac43436-sdio.txt"
+	ln -sf cyfmac43455-sdio-standard.bin "$(TARGET_DIR)/lib/firmware/cypress/cyfmac43455-sdio.bin"
 endef
 
 $(eval $(generic-package))
