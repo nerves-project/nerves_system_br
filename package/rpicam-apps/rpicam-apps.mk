bf7a86932999035ec79f16561f8c87bda72b2b21
Frank Hunleth <fhunleth@troodon-software.com>
2024-03-11 00:42:23 -0400

    rpicam-apps: new package
    This is the new name for libcamera-apps. It is also the latest and
    greated version.

---
+++ package/rpicam-apps/rpicam-apps.mk
@@ -0,0 +1,55 @@
+################################################################################
+#
+# rpicam-apps
+#
+################################################################################
+
+RPICAM_APPS_VERSION = 1.4.3
+RPICAM_APPS_SITE = $(call github,raspberrypi,rpicam-apps,v$(RPICAM_APPS_VERSION))
+RPICAM_APPS_LICENSE = BSD-2-Clause
+RPICAM_APPS_LICENSE_FILES = license.txt
+RPICAM_APPS_DEPENDENCIES = \
+	host-pkgconf \
+	boost \
+	jpeg \
+	rpi-libcamera \
+	libexif \
+	libpng \
+	tiff
+
+RPICAM_APPS_CONF_OPTS = \
+	-Denable_opencv=false \
+	-Denable_tflite=false
+
+ifeq ($(BR2_PACKAGE_LIBDRM),y)
+RPICAM_APPS_DEPENDENCIES += libdrm
+RPICAM_APPS_CONF_OPTS += -Denable_drm=true
+else
+RPICAM_APPS_CONF_OPTS += -Denable_drm=false
+endif
+
+ifeq ($(BR2_PACKAGE_FFMPEG)$(BR2_PACKAGE_LIBDRM),yy)
+RPICAM_APPS_DEPENDENCIES += ffmpeg libdrm
+RPICAM_APPS_CONF_OPTS += -Denable_libav=true
+else
+RPICAM_APPS_CONF_OPTS += -Denable_libav=false
+endif
+
+ifeq ($(BR2_PACKAGE_XORG7),y)
+RPICAM_APPS_DEPENDENCIES += \
+	$(if $(BR2_PACKAGE_LIBEPOXY),libepoxy) \
+	$(if $(BR2_PACKAGE_XLIB_LIBX11),xlib_libX11)
+endif
+
+ifeq ($(BR2_PACKAGE_QT5),y)
+RPICAM_APPS_DEPENDENCIES += qt5base
+RPICAM_APPS_CONF_OPTS += -Denable_qt=true
+else
+RPICAM_APPS_CONF_OPTS += -Denable_qt=false
+endif
+
+ifeq ($(BR2_TOOLCHAIN_HAS_LIBATOMIC),y)
+#RPICAM_APPS_CONF_OPTS += -DCMAKE_EXE_LINKER_FLAGS=-latomic
+endif
+
+$(eval $(meson-package))
