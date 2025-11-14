34566797b805cd31b91d71acac919054f580f455
Frank Hunleth <fhunleth@troodon-software.com>
2025-11-14 10:07:25 -0500

    rpi-distro-firmware-nonfree: bump to 1:20241210-1+rpt4
    From the changelog:
    
    ```
    firmware-nonfree (1:20241210-1+rpt4) trixie; urgency=medium
    
      * Add brcmfmac module parameters for stability
    
     -- Serge Schneider <serge@raspberrypi.com>  Wed, 12 Nov 2025 16:36:31 +0000
    ```
    
    TL;DR - these options were added to modprobe.d:
    
    ```
    options brcmfmac roamoff=1 feature_disable=0x282000
    ```

--- package/rpi-distro-firmware-nonfree/rpi-distro-firmware-nonfree.mk
+++ package/rpi-distro-firmware-nonfree/rpi-distro-firmware-nonfree.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_DISTRO_FIRMWARE_NONFREE_VERSION = c9d3ae6584ab79d19a4f94ccf701e888f9f87a53 # 1:20240709-2~bpo12+1+rpt3
+RPI_DISTRO_FIRMWARE_NONFREE_VERSION = 40dea60e27078fac57a3fed51010e2c26865d49b # 1:20241210-1+rpt4
 RPI_DISTRO_FIRMWARE_NONFREE_SITE = $(call github,RPi-Distro,firmware-nonfree,$(RPI_DISTRO_FIRMWARE_NONFREE_VERSION))
 RPI_DISTRO_FIRMWARE_NONFREE_LICENSE_FILES = debian/copyright
 
@@ -12,10 +12,14 @@ RPI_DISTRO_FIRMWARE_NONFREE_LICENSE_FILES = debian/copyright
 # 1. The cyfmac43455-sdio.bin symlink comes from the firmware-brcm80211.postinst script
 # 2. The cyfmac43455-sdio-minimal firmware isn't used. It's for increasing the number
 #    of supported clients in AP mode by removing functionality.
+# 3. The rpi-brcmfmac.conf file has parameter settings that reportedly fix a
+#    stability issue seen upstream
 define RPI_DISTRO_FIRMWARE_NONFREE_INSTALL_TARGET_CMDS
 	mkdir -p "$(TARGET_DIR)/lib/firmware"
+	mkdir -p "$(TARGET_DIR)/lib/modprobe.d"
 	cp -dpfr "$(@D)/debian/config/brcm80211/brcm" "$(TARGET_DIR)/lib/firmware"
 	cp -dpfr "$(@D)/debian/config/brcm80211/cypress" "$(TARGET_DIR)/lib/firmware"
+	cp -dpf "$(@D)/debian/rpi-brcmfmac.conf" "$(TARGET_DIR)/lib/modprobe.d"
 	rm -f "$(TARGET_DIR)/lib/firmware/cypress/cyfmac43455-sdio-minimal.bin"
 	rm -f "$(TARGET_DIR)/lib/firmware/cypress/README.txt"
 	ln -sf cyfmac43455-sdio-standard.bin "$(TARGET_DIR)/lib/firmware/cypress/cyfmac43455-sdio.bin"
